f7f247cd3eab452a5bd66fe31ce92a1e5fc90b74
Brad Garropy <bradgarropy@gmail.com>
2017-07-19 22:40:03 -0500

    Using port based on environment for Heroku deployment.

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.use("/user", user);
 
 
 // start application
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, function() {
     console.log("Server listening on port %s.", port);
 });
